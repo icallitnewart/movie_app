508f7deab8553bdba55ff39ee812ec65c37108b8
icallitnewart <65598117+icallitnewart@users.noreply.github.com>
2020-08-10 02:04:46 +0900

    Updates

---
+++ precache-manifest.409da7455e1a3f4a3b9e3faacd2be540.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c34df3f30a8536b55f6d14de5e61c084",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "b181f816c8828d06ed9d",
+    "url": "/movie_app/static/css/main.251456b7.chunk.css"
+  },
+  {
+    "revision": "c0071c60e1a78cbbff2e",
+    "url": "/movie_app/static/js/2.ce898f91.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app/static/js/2.ce898f91.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b181f816c8828d06ed9d",
+    "url": "/movie_app/static/js/main.c75a2b3d.chunk.js"
+  },
+  {
+    "revision": "8a294428b8499a7b0c17",
+    "url": "/movie_app/static/js/runtime-main.9ac0a6e2.js"
+  }
+]);
